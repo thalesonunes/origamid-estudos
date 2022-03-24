10a6db512f87345a66514b3d3314c886f0ab465f
Thales Nunes <thales.o.nunes@gmail.com>
2022-03-24 09:26:10 -0300

    Desafio Componentes - Resolvido!

--- appreact/src/App.js
+++ appreact/src/App.js
@@ -1,4 +1,7 @@
 import React from "react";
+import Header from "./exe03/Header";
+import Home from "./exe03/Home";
+import Produtos from "./exe03/Produtos";
 
 
 // EXERCÍCIO 01
@@ -78,46 +81,74 @@ import React from "react";
 
 // Organize os produtos como mostrado no vídeo
 // Mostre apenas produtos que forem mais caros que R$ 1500
-const produtos = [
-  {
-    id: 1,
-    nome: 'Smartphone',
-    preco: 'R$ 2000',
-    cores: ['#29d8d5', '#252a34', '#fc3766'],
-  },
-  {
-    id: 2,
-    nome: 'Notebook',
-    preco: 'R$ 3000',
-    cores: ['#ffd045', '#d4394b', '#f37c59'],
-  },
-  {
-    id: 3,
-    nome: 'Tablet',
-    preco: 'R$ 1500',
-    cores: ['#365069', '#47c1c8', '#f95786'],
-  },
-];
+
+// const produtos = [
+//   {
+//     id: 1,
+//     nome: 'Smartphone',
+//     preco: 'R$ 2000',
+//     cores: ['#29d8d5', '#252a34', '#fc3766'],
+//   },
+//   {
+//     id: 2,
+//     nome: 'Notebook',
+//     preco: 'R$ 3000',
+//     cores: ['#ffd045', '#d4394b', '#f37c59'],
+//   },
+//   {
+//     id: 3,
+//     nome: 'Tablet',
+//     preco: 'R$ 1500',
+//     cores: ['#365069', '#47c1c8', '#f95786'],
+//   },
+// ];
+
+// const App = () => {
+
+//   const dados = produtos.filter(
+//     ({preco}) => Number(preco.replace("R$ ", "")) > 1500);
+
+//   return <section>
+//     {dados.map(({id, nome, preco, cores}) => 
+//     <div key={id}>
+//       <h1>{nome}</h1>
+//       <p>Preço: {preco}</p>
+//       <ul>
+//       {cores.map((cor) => (
+//           <li key={cor} style={{ backgroundColor: cor, color: 'white', listStyleType: "none" }}>
+//             {cor}
+//           </li>
+//         ))}
+//       </ul>
+//     </div> )}
+//   </section>;
+// };
+
+
+// EXERCÍCIO 03
+
+// Replique a interface como a apresentada na aula
+// Utilize a array abaixo para mostrar os produtos
+// Quebre em componentes o que precisar ser reutilizado
+// Dica: const { pathname } = window.location; (puxa o caminho do URL)
 
 const App = () => {
 
-  const dados = produtos.filter(
-    ({preco}) => Number(preco.replace("R$ ", "")) > 1500);
+  let Pagina = Home;
+  const {pathname} = window.location;
 
-  return <section>
-    {dados.map(({id, nome, preco, cores}) => 
-    <div key={id}>
-      <h1>{nome}</h1>
-      <p>Preço: {preco}</p>
-      <ul>
-      {cores.map((cor) => (
-          <li key={cor} style={{ backgroundColor: cor, color: 'white', listStyleType: "none" }}>
-            {cor}
-          </li>
-        ))}
-      </ul>
-    </div> )}
-  </section>;
+  if(pathname === "/produtos"){
+    Pagina = Produtos;
+  }else{
+    Pagina = Home;
+  }
+
+  return ( 
+    <>
+      <Header />
+      <Pagina />
+    </>
+  );
 };
 
 export default App;
